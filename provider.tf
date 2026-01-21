10cebfbec116442bf338971445be1a7292982070
rajatdeloitte7 <163399968+rajatdeloitte7@users.noreply.github.com>
2026-01-21 16:00:34 +0530

    Add provider.tf file

--- provider.tf
+++ provider.tf
@@ -1,8 +1,35 @@
-terraform {  
-  required_providers {  
-    aws = {  
-      source  = "hashicorp/aws"  
-      version = "~> 3.0"  
-    }  
-  }  
+# Provider configuration
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# Include module references
+
+module "iam_identity_center" {
+  source = "./modules/iam-identity-center"
+}
+
+module "s3_buckets" {
+  source = "./modules/s3-buckets"
+}
+
+module "iam_policies_roles" {
+  source = "./modules/iam-policies-roles"
+}
+
+module "amazon_q_business" {
+  source = "./modules/amazon-q-business"
+}
+
+module "lambda_ingestion" {
+  source = "./modules/lambda-ingestion"
+}
+
+module "lambda_output" {
+  source = "./modules/lambda-output"
+}
+
+module "sqs_queue" {
+  source = "./modules/sqs-queue"
 }
